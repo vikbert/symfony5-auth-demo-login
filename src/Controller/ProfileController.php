018a07094162086886956c01199760ca46a069eb
vikbert <segentor@gmail.com>
2021-03-07 21:30:31 +0100

    updates

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -5,18 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
-    {
+    public function index(Security $security
+    ): Response {
         $this->denyAccessUnlessGranted('ROLE_USER');
 
         return $this->render(
             'profile/index.html.twig',
             [
-                'controller_name' => 'ProfileController',
+                'user' => $security->getUser(),
             ]
         );
     }
